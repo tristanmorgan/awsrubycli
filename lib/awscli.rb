6d8b67c8c5f8708bd0535c4640c57e02d053545d
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2021-07-12 16:20:02 +1000

    Adding KMS key list and create

--- lib/awscli.rb
+++ lib/awscli.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'aws-sdk-ec2'
+require 'aws-sdk-kms'
 require 'aws-sdk-s3'
 require 'thor'
 require 'json'
@@ -18,13 +19,46 @@ end
 
 module Awscli
   # sts sub commands
+  class Kms < SubCommandBase
+    map ['list-keys'] => :list_keys
+    map ['create-key'] => :create_key
+
+    # aws kms list-keys
+    desc 'list-keys', 'list-keys'
+    method_option :endpoint, type: :string, desc: 'Endpoint to connect to'
+    def list_keys
+      client = Aws::KMS::Client.new(options[:endpoint] ? { endpoint: options[:endpoint] } : {})
+      resp = client.list_keys(
+        {
+        }
+      )
+
+      puts JSON.pretty_generate(resp.to_h)
+    end
+
+    # aws kms create-key
+    desc 'create-key', 'create-key'
+    method_option :endpoint, type: :string, desc: 'Endpoint to connect to'
+    def create_key
+      client = Aws::KMS::Client.new(options[:endpoint] ? { endpoint: options[:endpoint] } : {})
+      resp = client.create_key(
+        {
+        }
+      )
+
+      puts JSON.pretty_generate(resp.to_h)
+    end
+  end
+
+  # sts sub commands
   class Sts < SubCommandBase
     map ['get-caller-identity'] => :get_caller_identity
 
     # aws sts get-caller-identity
     desc 'get-caller-identity', 'get-caller-identity'
+    method_option :endpoint, type: :string, desc: 'Endpoint to connect to'
     def get_caller_identity
-      client = Aws::STS::Client.new
+      client = Aws::STS::Client.new(options[:endpoint] ? { endpoint: options[:endpoint] } : {})
       resp = client.get_caller_identity(
         {
         }
@@ -38,8 +72,9 @@ module Awscli
   class S3 < SubCommandBase
     # aws s3 ls s3://teamvibrato/hashicorp/consul/
     desc 'ls BUCKET [PREFIX]', 'list objects in a bucket'
+    method_option :endpoint, type: :string, desc: 'Endpoint to connect to'
     def ls(bucket, prefix = nil)
-      client = Aws::S3::Client.new
+      client = Aws::S3::Client.new(options[:endpoint] ? { endpoint: options[:endpoint] } : {})
       resp = if prefix
                client.list_objects_v2(
                  bucket: bucket,
@@ -55,15 +90,17 @@ module Awscli
     end
 
     desc 'pressign BUCKET KEY', 'generate a presigned URL for BUCKET and KEY'
+    method_option :endpoint, type: :string, desc: 'Endpoint to connect to'
     def presign(bucket, key)
-      signer = Aws::S3::Presigner.new
+      signer = Aws::S3::Presigner.new(options[:endpoint] ? { endpoint: options[:endpoint] } : {})
       url = signer.presigned_url(:get_object, bucket: bucket, key: key)
       puts url
     end
 
     desc 'download BUCKET KEY PATH', 'Download to a PATH for BUCKET and KEY'
+    method_option :endpoint, type: :string, desc: 'Endpoint to connect to'
     def download(bucket, key, path)
-      client = Aws::S3::Client.new
+      client = Aws::S3::Client.new(options[:endpoint] ? { endpoint: options[:endpoint] } : {})
       File.open(path, 'wb') do |file|
         client.get_object(bucket: bucket, key: key) do |chunk|
           file.write(chunk)
@@ -79,8 +116,9 @@ module Awscli
     map ['get-windows-password'] => :get_windows_password
 
     desc 'describe-instances TAG', 'get instances with tag'
+    method_option :endpoint, type: :string, desc: 'Endpoint to connect to'
     def describe_instances(tag)
-      client = Aws::EC2::Client.new
+      client = Aws::EC2::Client.new(options[:endpoint] ? { endpoint: options[:endpoint] } : {})
       resp = client.describe_instances(
         filters: [
           {
@@ -96,8 +134,9 @@ module Awscli
     end
 
     desc 'describe-images TAG', 'describe images with tag'
+    method_option :endpoint, type: :string, desc: 'Endpoint to connect to'
     def describe_images(tag)
-      client = Aws::EC2::Client.new
+      client = Aws::EC2::Client.new(options[:endpoint] ? { endpoint: options[:endpoint] } : {})
       resp = client.describe_images(
         filters: [
           {
@@ -113,8 +152,9 @@ module Awscli
     end
 
     desc 'get-windows-password instance_id pem_path', 'Gets the windows password for an instance'
+    method_option :endpoint, type: :string, desc: 'Endpoint to connect to'
     def get_windows_password(instance_id, pem_path)
-      ec2 = Aws::EC2::Client.new
+      ec2 = Aws::EC2::Client.new(options[:endpoint] ? { endpoint: options[:endpoint] } : {})
       encrypted_password = ec2.get_password_data(instance_id: instance_id).password_data
       private_key = OpenSSL::PKey::RSA.new(File.read(pem_path))
       decoded = Base64.decode64(encrypted_password)
@@ -133,6 +173,9 @@ module Awscli
 
     desc 'ec2 SUBCOMMAND', 'run ec2 commands'
     subcommand 'ec2', Ec2
+
+    desc 'kms SUBCOMMAND', 'run kms commands'
+    subcommand 'kms', Kms
 
     desc 'sts SUBCOMMAND', 'run sts commands'
     subcommand 'sts', Sts
